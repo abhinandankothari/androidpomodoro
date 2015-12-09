3e2340b6e914367dd7b39f4adecc133a7bce38e3
Abhinandan Kothari <abhinandankothari@gmail.com>
2015-12-09 16:04:12 +0530

    Local Broadcast Manager

--- app/src/main/java/com/random/pomodoro/MainActivity.java
+++ app/src/main/java/com/random/pomodoro/MainActivity.java
@@ -1,6 +1,11 @@
 package com.random.pomodoro;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     private PomodoroOnClickListener listener;
 
+    BroadcastReceiver receiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +35,31 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         listener = new PomodoroOnClickListener();
         button.setOnClickListener(listener);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, final Intent intent) {
+                if (intent.getStringExtra("update_type") == "tick")
+                    textView.setText(String.format("%02d:%02d", intent.getLongExtra("elapsed_time_mins", 0), intent.getLongExtra("elapsed_time_secs", 0)));
+                else {
+                    Toast.makeText(MainActivity.this, "Pomodoro Successful", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver,
+                new IntentFilter(PomodoroTimerTask.TIMER_ACTION));
     }
 
     @Override
@@ -51,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
     private class PomodoroTimerTask extends TimerTask {
         public static final int NUMBER_OF_MILLISECONDS_IN_A_SECOND = 1000;
         public static final int NUMBER_OF_SECONDS_IN_MIN = 60;
+        public static final String TIMER_ACTION = "com.random.pomodoro.timer_broadcast";
+
         private final long startTime;
 
         public PomodoroTimerTask(long startTimeParam) {
@@ -59,29 +93,30 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
+            Intent intent = new Intent(TIMER_ACTION);
             long currentTime = new Date().getTime();
             long elapsedTimeMillis = currentTime - startTime;
             Log.d("APP_LOG", "TOTAL_TIME " + elapsedTimeMillis);
             if (elapsedTimeMillis <= TOTAL_TIME) {
                 final long elapsedTimeMins = elapsedTimeMillis / NUMBER_OF_MILLISECONDS_IN_A_SECOND / NUMBER_OF_SECONDS_IN_MIN;
                 final long elapsedTimeSecs = (elapsedTimeMillis / NUMBER_OF_MILLISECONDS_IN_A_SECOND) % NUMBER_OF_SECONDS_IN_MIN;
-                Log.d("APP_LOG", String.format("%02d:%02d", elapsedTimeMins, elapsedTimeSecs));
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText(String.format("%02d:%02d", elapsedTimeMins, elapsedTimeSecs));
-                    }
-                });
-            } else {
-                runOnUiThread(new Runnable() {
+                //Log.d("APP_LOG", String.format("%02d:%02d", elapsedTimeMins, elapsedTimeSecs));
+                intent.putExtra("update_type", "tick");
+                intent.putExtra("elapsed_time_mins", elapsedTimeMins);
+                intent.putExtra("elapsed_time_secs", elapsedTimeSecs);
 
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "Pomodoro Successful", Toast.LENGTH_LONG).show();
-                    }
-                });
+            } else {
+                intent.putExtra("update_type", "finish");
+//                runOnUiThread(new Runnable() {
+//
+//                    @Override
+//                    public void run() {
+//                        Toast.makeText(MainActivity.this, "Pomodoro Successful", Toast.LENGTH_LONG).show();
+//                    }
+                //});
                 listener.complete();
             }
+            LocalBroadcastManager.getInstance(MainActivity.this).sendBroadcast(intent);
         }
     }
 
